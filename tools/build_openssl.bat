f95990796a3a91a05615639b0ad29838efe5c7b9
jagannatharjun <jagannatharjun11@gmail.com>
2020-04-06 23:37:48 +0530

    fix openssl

--- tools/build_openssl.bat
+++ tools/build_openssl.bat
@@ -1,4 +1,4 @@
-@echo off
+@echo on
 setlocal
 
 set SSLRELEASE=1.0.x
@@ -10,44 +10,34 @@ echo Configuring OpenSSL %SSLVER% for %SSLARCH%...
 IF exist %SSLINSTALLDIR% (
     echo OpenSSL already installed!
     echo Delete or rename %SSLINSTALLDIR% if you want to rebuild it.
+	goto :eof
+)
+
+cd %SSLBUILDDIR%
+
+IF %SSLRELEASE% == 1.1.x (
+	rd build /s /q
+	md build
+	cd build
+
+	perl ..\configure VC-%SSLARCH% enable-static-engine no-shared --prefix=%SSLINSTALLDIR%
+
+	echo Building / Installing OpenSSL...
+	nmake install_sw
+	IF %errorlevel% NEQ 0 exit /b %errorlevel%
+
 ) ELSE (
-    IF exist %SSLBUILDDIR% (
+	perl Configure VC-%SSLARCH% enable-static-engine --prefix=%SSLINSTALLDIR%
+	IF %SSLARCH% == WIN32 start /W /BELOWNORMAL "Configuring OpenSSL 2/2..." ms\do_nasm ^&^& exit
+	IF %SSLARCH% == WIN64A start /W /BELOWNORMAL "Configuring OpenSSL 2/2..." ms\do_win64a ^&^& exit
 
-        cd %SSLBUILDDIR%
+	echo Building OpenSSL...
+	nmake -f ms\nt.mak clean all
+	IF %errorlevel% NEQ 0 exit /b %errorlevel%
 
-        IF %SSLRELEASE% == 1.1.x (
-
-            rd build /s /q
-            md build
-            cd build
-
-            perl ..\Configure VC-%SSLARCH% enable-static-engine no-shared --prefix=%SSLINSTALLDIR%
-
-            echo Building / Installing OpenSSL...
-            nmake install_sw
-            IF %errorlevel% NEQ 0 exit /b %errorlevel%
-
-        ) ELSE (
-
-            start /W /BELOWNORMAL "Configuring OpenSSL 1/2..." perl Configure VC-%SSLARCH% enable-static-engine --prefix=%SSLINSTALLDIR%
-            IF %SSLARCH% == WIN32 start /W /BELOWNORMAL "Configuring OpenSSL 2/2..." ms\do_nasm ^&^& exit
-            IF %SSLARCH% == WIN64A start /W /BELOWNORMAL "Configuring OpenSSL 2/2..." ms\do_win64a ^&^& exit
-
-            echo Building OpenSSL...
-            nmake -f ms\nt.mak clean all
-            IF %errorlevel% NEQ 0 exit /b %errorlevel%
-
-            echo Installing OpenSSL...
-            nmake -f ms\nt.mak install
-            IF %errorlevel% NEQ 0 exit /b %errorlevel%
-
-        )
-
-    ) ELSE (
-
-        echo Could not find OpenSSL sources in %SSLBUILDDIR%
-        exit /b 1
-    )
+	echo Installing OpenSSL...
+	nmake -f ms\nt.mak install
+	IF %errorlevel% NEQ 0 exit /b %errorlevel%
 )
 
 endlocal
